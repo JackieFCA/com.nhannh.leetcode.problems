77b0489481d6af27fae3089ad35e99c5f8e4e41e
Nhan Nguyen Hoang <nhan.nguyenhoang1@nashtechglobal.com>
2022-07-17 22:27:22 +0700

    Problem LCP-1 Two Sum
    Refactor code

--- src/main/java/problems/easy/TwoSumSolution.java
+++ src/main/java/problems/easy/TwoSumSolution.java
@@ -6,14 +6,12 @@ import java.util.Map;
 public class TwoSumSolution {
 
     public int[] twoSumOnePassHashMapTable(int[] nums, int target) {
-        // validation step
-        if (nums.length < 2 || nums.length > Math.pow(10, 4) || target < Math.pow(10, -9) || target > Math.pow(10, 9)) {
+        if (this.validate(nums, target)) {
             return new int[]{};
         }
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            // validation element
             if (nums[i] < Math.pow(10, -9) || nums[i] > Math.pow(10, 9)) {
                 return new int[]{};
             }
@@ -28,8 +26,7 @@ public class TwoSumSolution {
     }
 
     public int[] twoSumWith2Loops(int[] nums, int target) {
-        // validation step
-        if (nums.length < 2 || nums.length > Math.pow(10, 4) || target < Math.pow(10, -9) || target > Math.pow(10, 9)) {
+        if (this.validate(nums, target)) {
             return new int[]{};
         }
 
@@ -45,5 +42,10 @@ public class TwoSumSolution {
             }
         }
         return new int[]{};
+    }
+
+    private boolean validate(int[] nums, int target) {
+        return (nums.length < 2 || nums.length > Math.pow(10, 4)
+                || target < Math.pow(10, -9) || target > Math.pow(10, 9)) ? true : false;
     }
 }
